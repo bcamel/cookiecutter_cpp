47a5825176f5ff54465bc06454dfd018c7cd21bb
Matthias Siemering <matthias.siemering@gmail.com>
2021-11-14 15:31:18 +0100

    [chore] various fixes

--- {{ cookiecutter.repo_name }}/src/main.cpp
+++ {{ cookiecutter.repo_name }}/src/main.cpp
@@ -1,7 +1,8 @@
-#include <{{ cookiecutter.repo_name }}/logger.h>
+#include "{{ cookiecutter.repo_name }}/logger.h"
 
-int main(int argc, const char *argv[]) {
-  (void)(argc);
-  (void)(argv);
-  {{ cookiecutter.repo_name }}::getLogger("main")->info("Hello, world!");
+int main(int argc, const char *argv[])
+{
+    (void)argc;
+    (void)argv;
+    {{ cookiecutter.repo_name }}::getLogger("main")->info("Hello, world!");
 }
